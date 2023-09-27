f21948626f8ad75fb173034f131c75bae79dce25
Aleks <G00396018@atu.ie>
2023-09-27 12:28:28 +0100

    fixed and added break; for @RequestParam

--- src/main/java/ie/atu/Lab2/CalculatorController.java
+++ src/main/java/ie/atu/Lab2/CalculatorController.java
@@ -1,27 +1,28 @@
 package ie.atu.Lab2;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CalculatorController {
 
-    @GetMapping("/calculate/")
+    @GetMapping("/calculate")
     @ResponseBody
     public String calculator(@RequestParam int num1, @RequestParam int num2, @RequestParam String operation){
         int ans = 0;
         switch (operation){
             case "add":
                 ans = num1+num2;
+                break;
             case "subtract":
                 ans = num1-num2;
+                break;
             case "multiply":
                 ans = num1*num2;
+                break;
             case "divide":
                 ans = num1/num2;
+                break;
         }
-        return "Operation: " + operation + " total: " +ans;
+        return "Operation: " + operation + " total: " + ans;
     }
 }
